dd862c8a86ab8e4acdcf4137a3aa1b673f9c33e2
SwordShieldMouse <alke8882000@gmail.com>
2018-11-10 15:31:30 -0700

    added undersampled dataset, started oversampled dataset

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import numpy as np
 import sklearn as sk
+import pandas as pd
 import utils
 
 # set the random seed
 np.random.seed(566)
 
+# import data
+all_data = pd.read_csv("all-data.csv")
 
-# split the data
+
+# split the data into test, train, and validation sets
+test_size = 0.2 # percent of the dataset that should be test
+
+# try oversampling the minority class
+
+# try undersampling the majority class
