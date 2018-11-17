07d2d6a9afc0e3108bf282cd2421240f8e2a3265
SwordShieldMouse <alke8882000@gmail.com>
2018-11-17 13:51:55 -0700

    added Sounak's code to main.py

--- algorithms.py
+++ algorithms.py
@@ -12,7 +12,7 @@ def print_conf_matrix(algo_name, conf_mat):
 	print(conf_mat)
 
 def run_svm(train_X, train_y, test_X, test_y, kernel = 'rbf', gamma='auto', random_state = None):
-	
+
 	clf = SVC(kernel = kernel, gamma = gamma, random_state = random_state)
 	clf.fit(train_X, train_y)
 
@@ -37,3 +37,13 @@ def run_lr(train_X, train_y, test_X, test_y, solver = 'liblinear', random_state 
 	pred_y = clf.predict(test_X)
 	print_conf_matrix('Logistic Regression', pd.crosstab(test_y, pred_y, margins = True))
 	return
+
+def cross_validate_error(alg, X, y, kf):
+    # returns cross-validation error
+    # assumes we have been given an alg from scikit learn, so that we can call fit() and predict()
+    total_error = 0
+    for train_ix, test_ix in kf.split(X):
+        alg.fit(X[train_ix], y[train_ix])
+        preds = alg.predict(X[test_ix])
+        total_error += sum([a == b for a, b in zip(y[test_ix], preds)])
+    return total_error
--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import numpy as np
-import sklearn as sk
 import pandas as pd
-import utils
-from sk.model_selection import train_test_split
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import KFold
+import algorithms
 
-from sklearn.naive_bayes import BernouilliNB
+from sklearn.model_selection import train_test_split
+
+#from imblearn.over_sampling import SMOTE
+#from sklearn.model_selection import KFold
+
+#from sklearn.naive_bayes import BernouilliNB
 
 
 # set the random seed
 np.random.seed(566)
 
 ## Prepare the data
+## Prepare the data
 # import data
 all_data = pd.read_csv("all-data.csv")
+all_data['Output'] = all_data.apply(lambda r: 1 if r['Outcome'] == 'Active' else 0, axis = 1)
+all_data.drop('Outcome', inplace = True, axis = 1)
+
+
+# split the data into test and train sets
+X_data, y_data = all_data.loc[:, all_data.columns != 'Output'], all_data['Output']
+
+train_X, test_X, train_y, test_y = train_test_split(X_data, y_data, test_size = test_size, shuffle = True)
+print(train_X.shape, test_X.shape, train_y.shape, test_y.shape)
+
 p = all_data.shape[1] - 1 # number of features
-majority_data = all_data.loc[, "inactive"]
-n_minority = all_data.loc[, "active"].shape[0] # number of minority samples
+
+majority_data = all_data.loc[all_data["Output"] == 0]
+n_minority = all_data.loc[all_data["Output"] == 1].shape[0] # number of minority samples
+print("There are " + str(n_minority) + " minority samples.")
 
 # split the data into test, train, and validation sets
-test_size = 0.3 # percent of the dataset that should be set aside for test
+test_size = 0.1 # percent of the dataset that should be set aside for test
 n_splits = 10 # number of splits for cross-validation
-# just doing a regular train/test
-train_X, train_y, test_X, test_y = train_test_split(all_data[, 1:p], undersampled[, -1], test_size = test_size, shuffle = True)
+
+# cross-validation for the regular train/test split
 kf = KFold(n_splits = n_splits)
 kf.get_splits(train_X)
 
@@ -39,10 +53,15 @@ kf.get_splits(train_X)
 # get as many majority samples as n_minority
 # both train and test sets should be relatively balanced
 undersampled_data = majority_data.sample(n = n_minority)
-undersampled_train_X, undersampled_train_y, undersampled_test_X, undersampled_test_y = train_test_split(undersampled_data[, 1:p], undersampled[, -1], test_size = test_size, shuffle = True)
+undersampled_train_X, undersampled_train_y, undersampled_test_X, undersampled_test_y = train_test_split(undersampled_data[:, 1:p], undersampled[:, -1], test_size = test_size, shuffle = True)
 undersampled_kf = KFold(n_splits = n_splits)
 undersampled_kf.get_splits(undersampled_train_X)
 
 
 
-## Perform the experiments
+# Run the experiments
+algorithms.run_svm(train_X, train_y, test_X, test_y)
+
+algorithms.run_nb(train_X, train_y, test_X, test_y)
+
+algorithms.run_lr(train_X, train_y, test_X, test_y)
