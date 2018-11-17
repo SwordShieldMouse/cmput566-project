a5c6b80ce8d7dcf79d8588129c51aff8fba63fd1
SwordShieldMouse <alke8882000@gmail.com>
2018-11-17 13:54:45 -0700

    fixed some bugs. should run now

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+
 import algorithms
 
+from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 
 #from imblearn.over_sampling import SMOTE
@@ -21,7 +23,10 @@ all_data['Output'] = all_data.apply(lambda r: 1 if r['Outcome'] == 'Active' else
 all_data.drop('Outcome', inplace = True, axis = 1)
 
 
+
 # split the data into test and train sets
+test_size = 0.1 # percent of the dataset that should be set aside for test
+n_splits = 10 # number of splits for cross-validation
 X_data, y_data = all_data.loc[:, all_data.columns != 'Output'], all_data['Output']
 
 train_X, test_X, train_y, test_y = train_test_split(X_data, y_data, test_size = test_size, shuffle = True)
@@ -33,13 +38,10 @@ majority_data = all_data.loc[all_data["Output"] == 0]
 n_minority = all_data.loc[all_data["Output"] == 1].shape[0] # number of minority samples
 print("There are " + str(n_minority) + " minority samples.")
 
-# split the data into test, train, and validation sets
-test_size = 0.1 # percent of the dataset that should be set aside for test
-n_splits = 10 # number of splits for cross-validation
 
 # cross-validation for the regular train/test split
 kf = KFold(n_splits = n_splits)
-kf.get_splits(train_X)
+kf.get_n_splits(train_X)
 
 # try oversampling the minority class
 # NOTE: Should only oversample on the training data to ensure independence between training and test sets
@@ -52,10 +54,10 @@ kf.get_splits(train_X)
 # try undersampling the majority class
 # get as many majority samples as n_minority
 # both train and test sets should be relatively balanced
-undersampled_data = majority_data.sample(n = n_minority)
+"""undersampled_data = majority_data.sample(n = n_minority)
 undersampled_train_X, undersampled_train_y, undersampled_test_X, undersampled_test_y = train_test_split(undersampled_data[:, 1:p], undersampled[:, -1], test_size = test_size, shuffle = True)
 undersampled_kf = KFold(n_splits = n_splits)
-undersampled_kf.get_splits(undersampled_train_X)
+undersampled_kf.get_splits(undersampled_train_X)"""
 
 
 
