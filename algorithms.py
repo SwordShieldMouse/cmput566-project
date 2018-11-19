d5872c333b9f95ee4aa8d82d0c1a1cdc3cc16e0b
SwordShieldMouse <alke8882000@gmail.com>
2018-11-18 23:15:07 -0700

    added some cv code that isn't working yet. added f1 score to algorithms.

--- algorithms.py
+++ algorithms.py
@@ -5,13 +5,15 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import sklearn.metrics
+from sklearn.metrics import f1_score
 
 #Taken from Assignment 3
 def getaccuracy(ytest, predictions):
 	correct = 0
 	for i in range(len(ytest)):
 		if ytest[i] == predictions[i]:
-		correct += 1
+			correct += 1
 	return (correct/float(len(ytest))) * 100.0
 
 #Taken from Assignment 3
@@ -26,17 +28,19 @@ def print_conf_matrix(algo_name, conf_mat):
 
 #SVM, where the kernel is passed from main
 #To Do: Cross validation to find best kernel
+# TODO: Add sample weights to deal with class imbalance
 def run_svm(train_X, train_y, test_X, test_y, kernel = 'rbf', gamma='auto', random_state = None):
 
-	clf = SVC(kernel = kernel, gamma = gamma, random_state = random_state)
+	clf = SVC(kernel = kernel, gamma = gamma, random_state = random_state, class_weight = "balanced")
 	clf.fit(train_X, train_y)
 
 	pred_y = clf.predict(test_X)
 	print_conf_matrix('SVM', pd.crosstab(test_y, pred_y, margins = True))
-	print('Error for SVM: ' + geterror(test_y, pred_y))
+	print('F1 score for SVM: ' + str(f1_score(test_y, pred_y)))
 	return
 
 #Naive Bayes prediction assuming features from bernoulli distribution
+# TODO: Add sample weights to deal with class imbalance
 def run_nb(train_X, train_y, test_X, test_y):
 
 	clf = BernoulliNB(alpha = 1.0, fit_prior = True)
@@ -44,27 +48,32 @@ def run_nb(train_X, train_y, test_X, test_y):
 
 	pred_y = clf.predict(test_X)
 	print_conf_matrix('Naive Bayes', pd.crosstab(test_y, pred_y, margins = True))
-	print('Error for Naive Bayes: ' + geterror(test_y, pred_y))
+	print('F1 score for Naive Bayes: ' + str(f1_score(test_y, pred_y)))
 	return
 
 #logistic regression with l1 regularization, where the regularization parameter is passed from main
 #To Do: Cross validation to find best parameter
+# TODO: Add sample weights to deal with class imbalance
 def run_lr(train_X, train_y, test_X, test_y, reg_param = 1.0, solver = 'liblinear', random_state = None):
 
-	clf = LogisticRegression(penalty = 'l1', C = 1.0/reg_param, solver = solver, random_state = random_state)
+	clf = LogisticRegression(penalty = 'l1', C = 1.0/reg_param, solver = solver, random_state = random_state, class_weight = "balanced")
 	clf.fit(train_X, train_y)
 
 	pred_y = clf.predict(test_X)
 	print_conf_matrix('Logistic Regression', pd.crosstab(test_y, pred_y, margins = True))
-	print('Error for Logistic Regression: ' + geterror(test_y, pred_y))
+	print('F1 score for Logistic Regression: ' + str(f1_score(test_y, pred_y)))
 	return
 
-def cross_validate_error(alg, X, y, kf):
-    # returns cross-validation error
-    # assumes we have been given an alg from scikit learn, so that we can call fit() and predict()
-    total_error = 0
-    for train_ix, test_ix in kf.split(X):
-        alg.fit(X[train_ix], y[train_ix])
-        preds = alg.predict(X[test_ix])
-        total_error += sum([a == b for a, b in zip(y[test_ix], preds)])
-    return total_error
+def cross_validate(algs, X, y, kf):
+	# assumes we have been given algs from scikit learn, so that we can call fit() and predict()
+	# assumes algs is a dictionary
+	# returns the F1 scores for all the algorithms
+	scores = {}
+	ixs = kf.split(X, y)
+	for name, alg in algs.items():
+		y_pred = np.array([])
+		for train_ix, test_ix in ixs:
+			alg.fit(X[train_ix], y[train_ix])
+			np.concatenate(y_pred, alg.predict(X[test_ix]))
+		scores[name] = f1_score(y_pred)
+	return scores
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 import algorithms
 
-from sklearn.model_selection import KFold
+from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 
 #from imblearn.over_sampling import SMOTE
@@ -30,17 +34,18 @@ n_splits = 10 # number of splits for cross-validation
 X_data, y_data = all_data.loc[:, all_data.columns != 'Output'], all_data['Output']
 
 train_X, test_X, train_y, test_y = train_test_split(X_data, y_data, test_size = test_size, shuffle = True)
-print(train_X.shape, test_X.shape, train_y.shape, test_y.shape)
+#print(train_X.shape, test_X.shape, train_y.shape, test_y.shape)
 
 p = all_data.shape[1] - 1 # number of features
 
 majority_data = all_data.loc[all_data["Output"] == 0]
 n_minority = all_data.loc[all_data["Output"] == 1].shape[0] # number of minority samples
+print("Total samples = " + str(all_data.shape[0]))
 print("There are " + str(n_minority) + " minority samples.")
 
 
 # cross-validation for the regular train/test split
-kf = KFold(n_splits = n_splits)
+kf = StratifiedKFold(n_splits = n_splits)
 kf.get_n_splits(train_X)
 
 # try oversampling the minority class
@@ -62,6 +67,26 @@ undersampled_kf.get_splits(undersampled_train_X)"""
 
 
 # Run the experiments
+# cross-validation experiments
+
+# cross-validation parameters
+regwgts = np.arange(1, 11, 1)
+kernels = ["linear", "rbf"]
+gammas = np.arange(0.05, 1.05, 0.05)
+lr_algs = {}
+svm_algs = {}
+for regwgt in regwgts:
+    lr_algs["Logistic Regression: regwgt = " + str(regwgt)] = LogisticRegression(penalty = 'l1', C = 1.0/regwgt, solver = "liblinear", random_state = None, class_weight = "balanced")
+for gamma in gammas:
+    svm_algs["SVM: RBG gamma = " + str(gamma)] = SVC(kernel = "rbf", gamma = gamma, random_state = None, class_weight = "balanced")
+
+# run cross-validation
+#cv_lr_scores = algorithms.cross_validate(lr_algs, train_X, train_y, kf)
+#cv_svm_scores = algorithms.cross_validate(svm_algs, train_X, train_y, kf)
+
+
+# final experiments (e.g., to get standard error)
+
 algorithms.run_svm(train_X, train_y, test_X, test_y)
 
 algorithms.run_nb(train_X, train_y, test_X, test_y)
